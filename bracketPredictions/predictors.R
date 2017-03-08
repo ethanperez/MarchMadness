311b9bd5216d4e6e85fda1f63887b2c44ac7afa9
Ethan Perez <ethan@ethanperez.com>
2017-03-08 00:46:37 -0600

    Added initial pass

---
+++ bracketPredictions/predictors.R
@@ -0,0 +1,32 @@
+require(aod)
+require(ggplot2)
+
+bracketData <- read.csv("~/Projects/MarchMadness/bracketPredictions/1986.2010.bracketData.csv")
+
+probitModel <- glm(
+  win ~ seed + opp_seed + 
+    win.percent + opp_win_percent + 
+    wins_in_last_10 + opp_wins_in_last_10 +
+    percent_of_wins_away + opp_percent_of_wins_away +
+    Sagarin + opp_Sagarin +
+    ppg + opp_Points_Scored_Per_Game +
+    ppg_allowed + opp_Points_Allowed_Per_Game, 
+  family = binomial(link = "probit"), 
+  data = bracketData
+)
+
+summary(probitModel)
+
+olsModel <- lm(
+  win ~ seed + opp_seed + 
+    win.percent + opp_win_percent + 
+    wins_in_last_10 + opp_wins_in_last_10 +
+    percent_of_wins_away + opp_percent_of_wins_away +
+    Sagarin + opp_Sagarin +
+    ppg + opp_Points_Scored_Per_Game +
+    ppg_allowed + opp_Points_Allowed_Per_Game, 
+  family = binomial(link = "probit"), 
+  data = bracketData
+)
+
+summary(olsModel)
